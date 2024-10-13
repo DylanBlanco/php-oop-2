9389cb59407f4fefe9dd36d0b3befbcb0768224d
DylanBlanco <dylanblanco1198@gmail.com>
2024-10-13 13:48:27 +0200

    create class category e product

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
-    $columnsNumber = 4;
+
+class Category {
+    public $name;
+    public $icon;
+
+    function __construct(string $name, string $icon) {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+}
+
+class Product {
+    public $title;
+    public $price;
+    public $image;
+    public $category;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>PHP OOP 2</title>
 
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -18,28 +34,11 @@
                 <div class="row">
                     <div class="col text-center">
                         <h1>
-                            Template PHP
+                            PHP OOP 2
                         </h1>
                     </div>
                 </div>
             </div>
         </header>
-
-        <main>
-            <div class="container">
-                <div class="row">
-                    <?php
-                        for ($i = 0; $i < $columnsNumber; $i++) {
-                    ?>
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                            Colonna <?php echo $i + 1; ?>
-                        </div>
-                    <?php
-                        }
-                    ?>
-                </div>
-            </div>
-        </main>
-
     </body>
 </html>
